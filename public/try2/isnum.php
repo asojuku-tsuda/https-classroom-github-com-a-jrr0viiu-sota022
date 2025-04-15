9d1f3ad5c1f6e308090be5e6d8ced16f08bf9534
sota022 <144978935+sota022@users.noreply.github.com>
2025-04-15 01:41:31 +0000

    2

--- public/try2/isnum.php
+++ public/try2/isnum.php
@@ -9,9 +9,11 @@
     <div class="login-box">
       <h2>
 <?php
-
-
+if(mb_ereg('^[0-9]+$',$_GET['indata'])==false){
+  die("数字を入力してください。");
+}else{
 echo "入力された数字は： " . $_GET['indata'];
+}
 ?>
     </h2>
     </div>
